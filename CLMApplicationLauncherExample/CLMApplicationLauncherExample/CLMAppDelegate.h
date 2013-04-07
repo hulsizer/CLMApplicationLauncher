f93cb9c0e92aa94c95db6a695c0990029deb6508
Andrew Hulsizer <andrew.hulsizer@gmail.com>
2013-04-07 01:40:43 -0500

    Initial Commit

---
+++ CLMApplicationLauncherExample/CLMApplicationLauncherExample/CLMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CLMAppDelegate.h
+//  CLMApplicationLauncherExample
+//
+//  Created by Andrew Hulsizer on 4/7/13.
+//  Copyright (c) 2013 Andrew Hulsizer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CLMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CLMApplicationLauncherExample/CLMApplicationLauncherExample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CLMApplicationLauncherExample
+//
+//  Created by Andrew Hulsizer on 4/7/13.
+//  Copyright (c) 2013 Andrew Hulsizer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CLMAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CLMAppDelegate class]));
+    }
+}
